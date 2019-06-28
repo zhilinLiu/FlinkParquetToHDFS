f103b566bf99de9200e0a8ba06795472ee373bf0
liuzhilin <1234567890>
2019-06-28 17:11:49 +0800

    6/28  17:11

--- src/main/java/Table/test.scala
+++ src/main/java/Table/test.scala
@@ -1,12 +1,17 @@
 package Table
 
+import java.io.File
 import java.net.{URI, URL}
 
-import org.apache.avro.generic.GenericRecord
+import io.eels.component.parquet.ParquetWriterConfig
+import org.apache.avro.Schema
+import org.apache.avro.generic.{GenericData, GenericRecord}
 import org.apache.hadoop.conf.Configuration
 import org.apache.hadoop.fs.{FileSystem, Path}
 import org.apache.hadoop.io.IOUtils
-import org.apache.parquet.avro.AvroParquetReader
+import org.apache.parquet.avro.{AvroParquetReader, AvroParquetWriter}
+import org.apache.parquet.hadoop.metadata.CompressionCodecName
+import org.apache.parquet.hadoop.{ParquetFileWriter, ParquetWriter}
 
 object test {
   def main(args: Array[String]): Unit = {
@@ -26,20 +31,54 @@ object test {
 //      outputStream.close()
 //    }
 
-    val reader = new AvroParquetReader[GenericRecord](new Path("d://b/1561706806774.parquet"))
-    var record:GenericRecord = null
+    val schema1 = "{\"namespace\":\"flinkRun\"," +
+      "             \"type\": \"record\"," +
+      "             \"name\": \"parquet_test\"," +
+      "             \"fields\": [{\"name\": \"name\", \"type\": [\"string\",\"null\"]}," +
+      "                          {\"name\": \"id\",\"type\": [\"int\",\"null\"]}" +
+      "             ]}  "
+    val schema: Schema = new Schema.Parser().parse(schema1)
+    val ccn = CompressionCodecName.SNAPPY
+    val config = ParquetWriterConfig()
+    val writer: ParquetWriter[GenericRecord] = AvroParquetWriter.builder[GenericRecord](new Path("d://all.parquet"))
+    .withSchema(schema)
+    .withCompressionCodec(ccn)
+    .withPageSize(config.pageSize)
+    .withRowGroupSize(config.blockSize)
+    .withDictionaryEncoding(config.enableDictionary)
+    .withWriteMode(ParquetFileWriter.Mode.OVERWRITE)
+    .withValidation(config.validating)
+    .build()
+    val gr: GenericRecord = new GenericData.Record(schema)
+    val files = new File("d://b")
+    val filelist = files.listFiles()
+    filelist.filter(f=>f.length()<1024*1024*18).foreach{f =>
+      val reader = new AvroParquetReader[GenericRecord](new Path(f.getPath))
+      var record:GenericRecord = null
+      var flag = true
+      while (flag){
+        record = reader.read()
+        if(record==null){
+          flag = false
+        }else{
+          gr.put("name",record.get("name"))
+          gr.put("id",record.get("id"))
+          try{
+          writer.write(gr)
+
+          }catch {
+            case e:Exception =>{
+              println("报错啦")
+            }
+          }
 
 
-    var flag = true
-    while (flag){
-      record = reader.read()
-      if(record==null){
-        flag = false
-      }else{
-        println(record)
-      }
-
+          println(record)
+        }
     }
 
+
+    }
+    writer.close()
   }
 }
